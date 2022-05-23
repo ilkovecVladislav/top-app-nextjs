775605a234d9a6c8f00e74fe56fb1bfb8ffa783a
Uladzislau Ilkavets <uladzislau_ilkavets@senla.eu>
2022-05-23 15:46:00 +0300

    Husky

---
+++ pages/index.tsx
@@ -0,0 +1,30 @@
+import { GetStaticProps } from "next";
+import { Button, Rating } from "../components";
+import { withLayout } from "../layout/Layout";
+import axios from "axios";
+import { MenuItem } from "../interfaces/menu.interface";
+
+const Home = ({ menu }: HomeProps): JSX.Element => {
+  return <></>;
+};
+
+export default withLayout(Home);
+
+export const getStaticProps: GetStaticProps = async () => {
+  const firstCategory = 0;
+  const { data: menu } = await axios.post<MenuItem[]>(
+    process.env.NEXT_PUBLIC_DOMAIN + "/api/top-page/find",
+    { firstCategory }
+  );
+  return {
+    props: {
+      menu,
+      firstCategory,
+    },
+  };
+};
+
+interface HomeProps extends Record<string, unknown> {
+  menu: MenuItem[];
+  firstCategory: number;
+}
